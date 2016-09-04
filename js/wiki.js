9a961f3f5f17859743e4d1246a86b4a89fa7a0d8
Curt Hill <nwres1git config --global user.email nwres1@yahoo.comgit config --global user.email nwres1>
2016-09-04 14:05:28 -0700

    handled no search results, style changes

--- js/wiki.js
+++ js/wiki.js
@@ -1,13 +1,11 @@
 var wikki = (function () {
+    var resultsDiv;
 
     var init = function () {
         $(function () {
 
-//            $("#btnToggleTemperature").click(toggleTemperature);
+            resultsDiv = $('#results');
             addEventHandlers();
-
-            //$.when(getWikki())
-            //.then(displayWikki);
                 
         })
     };
@@ -45,60 +43,34 @@ var wikki = (function () {
     }
 
     function displaySearchResults(searchResults) {
-        var resultsDiv = $('#results'),
-            newDiv,
-            article,
-            articleTitleForUrl,
-            articleLink;
-
         resultsDiv.empty();
 
-        for (var articleNumber = 0; articleNumber < 10; articleNumber++) {
-            newDiv = $('<div></div>');
-            article = searchResults.query.search[articleNumber];
+        if (0 === searchResults.query.search.length) {
+            displayHtml('your search returned no results');
+        }
+
+        searchResults.query.search.forEach(function (article) {
+            var articleTitleForUrl,
+                articleLink;
             articleTitleForUrl = article.title.replace(/\s/g, '_');
             articleLink =
                 "<a href=\"" +
                 "https://en.wikipedia.org/wiki/" +
-                articleTitleForUrl + 
+                articleTitleForUrl +
                 "\" target=\"_blank\">" +
                 "<b>" + article.title + ": " + "</b>" +
                 article.snippet +
                 "</a>"
-            newDiv.html(articleLink);
-            resultsDiv.append(newDiv);
-        }
+            displayHtml(articleLink);
+        });
     }
 
+    function displayHtml(htmlContent) {
+        var newDiv = $('<div></div>');
 
-    function getWikki() {
-    
-        var apiURL =
-                "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&callback=?";
-        
-        return $.ajax( {
-            url: apiURL,
-            dataType: 'json',
-            type: 'POST'
-//            ,headers: { 'Api-User-Agent': 'Example/1.0' }
-/*            ,success: function(wikkiData) {
-               successfullyGotWikki(wikkiData);
-            }*/
-        } );        
-
+        newDiv.html(htmlContent);
+        resultsDiv.append(newDiv);
     }
-
-    function displayWikki(wikkiData) {
-//        alert(wikkiData);
-        for (var p in wikkiData.query.pages["15580374"].revisions[0]) {
-            console.log(p);
-        }
-        $("#results").html(wikkiData.query.pages["15580374"].revisions[0]["*"]);
-/*        $("#weather").text(weatherDescription);
-
-        $("#imgWeatherIcon").attr("src", imageUrl);*/
-    }
-
 
     return {
         init: init
